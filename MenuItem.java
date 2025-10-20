6184777ed3f3a7b6dc80296dc7051c2a31f350e0
Christy Vattatharakooran Kunjachan <christyvk.chris@gmail.com>
2025-10-20 10:49:13 -0700

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -2,6 +2,7 @@ package restaurant;
 // MenuItem class
 public class MenuItem {
     private String name;
+    //Property name for the price is modified to priceInDollars
     private double priceInDollars; 
 
     public MenuItem(String name, double price) {
